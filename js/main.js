62352acae0a9cdd9d03e0afcfed99f9e70dfdbab
Sara1502 <moreirasaragds@gmail.com>
2026-03-10 14:59:40 -0300

    Add input de data

--- js/main.js
+++ js/main.js
@@ -18,12 +18,17 @@ async function manipularSubmissaoFormulario(event) {
   const id = document.getElementById("pensamento-id").value
   const conteudo = document.getElementById("pensamento-conteudo").value
   const autoria = document.getElementById("pensamento-autoria").value
+  const data =document.getElementById('pensamento-data').value
+
+  if(!validardata(data)){
+    alert('Não é permitido cadastro de datas futuras')
+  }
 
   try{ 
     if(id) {
-      await api.editarPensamento({ id, conteudo, autoria })
+      await api.editarPensamento({ id, conteudo, autoria, data })
     } else {
-      await api.salvarPensamento({ conteudo, autoria })
+      await api.salvarPensamento({ conteudo, autoria, data })
      
     }
     ui.renderizarPensamentos()
@@ -45,4 +50,10 @@ async function manipularBusca() {
   } catch (error) {
     alert('Erro ao realizar busca')
   }
+}
+
+function validardata(data) {
+  const dataAtual = new Date()
+  const dataInserida = new Date(data)
+  return dataInserida <= dataAtual
 }
